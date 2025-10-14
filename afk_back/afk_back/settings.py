1cae8bde5697aac417319c97abe36d5bcd040176
Aexizzz <ammelihov@gmail.com>
2025-10-14 15:08:54 +0500

    Add files via upload

--- afk_back/afk_back/settings.py
+++ afk_back/afk_back/settings.py
@@ -172,21 +172,17 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 }
 
-CORS_ALLOW_ALL_ORIGINS = True  # Временно для запуска
-# Или правильные настройки:
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://176.109.105.158",
-#     "http://localhost:5173", 
-#     "http://127.0.0.1:5173",
-#     "http://backend:8000",
-#     "http://frontend:80",
-#     "http://afkural.ru",
-#     "https://afkural.ru",
-# ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1", 
+    "http://frontend",
+    "http://backend:8000",
+    "http://176.109.105.158",
+    "http://afkural.ru",
+    "https://afkural.ru",
+]
 
 # Admin registration code
 ADMIN_REGISTRATION_CODE = 'AFK2024ADMIN'
-
